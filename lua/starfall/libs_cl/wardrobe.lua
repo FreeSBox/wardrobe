cafd821224ae7044ca14345b992ccda9bce27522
kit-katkittycat <kit-katkittycat@noreply.codeberg.org>
2026-09-05 21:23:59 +0500

    Fix my fucking shit

--- lua/starfall/libs_cl/wardrobe.lua
+++ lua/starfall/libs_cl/wardrobe.lua
@@ -17,8 +17,6 @@ SF.hookAdd("Wardrobe_PostSetModel")
 -- @param string wsid Workshop ID of new model
 -- @param table hands New model's viewmodel hands 
 SF.hookAdd("Wardrobe_PreSetModel")
-=======
->>>>>>> 8c4e1cf838de0511f1c8350daa25ff2695ed865d
 return function(instance)
     -- https://github.com/FreeSBox/freesbox/blob/fded92bb4f4bd323a294286d4a2c9c359cced0fe/lua/starfall/libs_sh/fsb.lua#L31
     local player_methods, player_meta = instance.Types.Player.Methods, instance.Types.Player
@@ -33,21 +31,13 @@ return function(instance)
     --- Gets the player's model Workshop ID from Wardrobe.
     -- @client
     -- @return number Workshop ID. nil if the player doesn't have a workshop model
-<<<<<<< HEAD
-    function player_methods:getWorkshopID()
-=======
     function player_methods:getWardrobeID()
->>>>>>> 8c4e1cf838de0511f1c8350daa25ff2695ed865d
         return getply(self).wardrobeWsid
     end
     --- Gets the player's model path from Wardrobe.
     -- @client
     -- @return string Model path. nil if the player doesn't have a workshop model
-<<<<<<< HEAD
-    function player_methods:getWorkshopModelPath()
-=======
     function player_methods:getWardrobeModel()
->>>>>>> 8c4e1cf838de0511f1c8350daa25ff2695ed865d
         return getply(self).wardrobe
     end
 end
